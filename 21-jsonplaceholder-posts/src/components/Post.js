8fb7563e34ef696e19e8829198a77e9c3eaedd1d
Skalolazka123 <skalolazka123@gmail.com>
2023-01-21 07:12:24 +0200

    21.01 works

---
+++ 21-jsonplaceholder-posts/src/components/Post.js
@@ -0,0 +1,13 @@
+function Post(props) {
+    const { id, title, userId, body } = props
+    return (
+        <div>
+            <small>{id}</small>
+
+            <h2>{title}</h2>
+            <p>{body}</p>
+            <h3>User ID: {userId}</h3>
+        </div>
+    )
+}
+export default Post
---
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+import Post from './Post'
+
+function Posts() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((res) => res.json())
+            .then((posts) => setPosts(posts))
+            .catch((error) => console.log(error.message))
+    }, [])
+
+    return (
+        <div>
+            {posts && posts.map((post) => <Post {...post} key={post.id} />)}
+        </div>
+    )
+}
+export default Posts
